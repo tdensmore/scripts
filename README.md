9d920fb8d06af04971203f6682f074ac2224a6a5
todd densmore <todd@boxboat.com>
2018-11-27 13:44:24 -0500

    Added mac setup script

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # scripts
 Bootstrap scripts for a variety of tasks
+
+## macsetup
+Contains script to install and setup a new mac
---
+++ macsetup/new-setup.sh
@@ -0,0 +1,55 @@
+# Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# FONTS - ~/Library/Fonts https://wesbos.com/programming-fonts/
+brew tap caskroom/fonts
+## Iconsolata
+brew cask install font-inconsolata font-inconsolata-for-powerline
+## Source Code Pro
+brew cask install font-source-code-pro font-source-code-pro-for-powerline
+## ANONYMOUS PRO
+brew cask install font-anonymous-pro
+## Hermit
+brew cask install font-hermit
+## Ubuntu
+brew cask install font-ubuntu
+
+# Zshell
+brew install zsh
+
+# Git
+brew install git
+
+# iTerm2
+brew install tmux
+brew cask install meld # diff tool
+brew cask install iterm2
+brew cask install slack
+brew cask install atom
+brew cask install dropbox
+brew cask install 1password
+brew cask install keepingyouawake
+brew cask install appcleaner
+brew cask install docker
+brew cask install google-chrome
+brew cask install virtualbox
+brew cask install virtualbox-extension-pack
+# iso
+# http://releases.ubuntu.com/18.04.1/ubuntu-18.04.1-live-server-amd64.iso?_ga=2.124398850.948042938.1543251609-1439906146.1543251609
+
+
+# vim
+brew install vim
+# vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#screensavers
+cp -r /RedPill.saver/. ~/Library/Screen\ Savers/
+
+# Office
+# InkScape
+
+# move ssh keys
+
+# Remove install files
+brew cleanup
